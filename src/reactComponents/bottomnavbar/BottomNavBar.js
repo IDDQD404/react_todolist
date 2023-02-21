b0e796d46d400a79cc5b395ce7443b2a3230a31f
IDDQD404 <93680153+IDDQD404@users.noreply.github.com>
2023-02-21 22:23:45 +0300

    Update BottomNavBar.js

--- src/reactComponents/bottomnavbar/BottomNavBar.js
+++ src/reactComponents/bottomnavbar/BottomNavBar.js
@@ -8,18 +8,12 @@ import { setAmount, selectCount } from "./PageSlicer";
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function BottomNavBar() {
-  /* */
   const page = useSelector(selectCount);
-  //   const [PageNumber, setPageNumber] = useState(1);
-  //   const incrementValue = Number(PageNumber) || 1;
-  //   useEffect(() => {}, [PageNumber]);
-  /* */
   const pagecount = useSelector(selectCount);
   const dispatch = useDispatch();
-  /* */
+
   return (
     <div>
-      {/* <h2>{page}</h2> */}
       <div className="HorizontalLine">
         {numbers.map((number) => (
           <button
